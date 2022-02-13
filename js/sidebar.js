0517fdac85786a733f870669ab028cbc5ded1d98
Abhijeet <abhijeetsinha43@gmail.com>
2022-02-13 19:02:24 +0530

    fix: responsiveness fixes, utility updates  and aside nav scrolling fix

--- js/sidebar.js
+++ js/sidebar.js
@@ -1,7 +1,26 @@
 const hamburgerBtn = document.querySelector(".bars");
 const sideBar = document.querySelector(".sidebar");
+const navbar = document.querySelector(".navbar");
+const closeBtn = document.querySelector("#cross");
+
+const openAside = () => {
+  sideBar.classList.add("sidebar-active");
+  closeBtn.classList.add("active");
+  document.body.classList.add("overflow");
+};
+
+const closeAside = () => {
+  sideBar.classList.remove("sidebar-active");
+  closeBtn.classList.remove("active");
+  document.body.classList.remove("overflow");
+};
 
 hamburgerBtn.addEventListener("click", () => {
-  hamburgerBtn.classList.toggle("fa-xmark");
-  sideBar.classList.toggle("sidebar-active");
+  openAside();
+});
+
+sideBar.addEventListener("click", (e) => {
+  if (e.target.id === "cross") {
+    closeAside();
+  }
 });
